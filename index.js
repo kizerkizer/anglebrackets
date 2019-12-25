d04aca6756f23fd5c8dbf6024e0a0049c1000990
Alex Kizer <kizerkizerkizerkizerkizer@gmail.com>
2019-12-25 14:13:24 -0600

    Remove tabs

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const sp = n => ` `.repeat(n);
 const escapes = ['& &amp;', '< &lt;', '> &gt;', '" &quot;', '\' &#039'];
 
 const escape = new Function('string', `${sp(2)}return string${escapes
-	.map(s => s.split(` `))
+  .map(s => s.split(` `))
   .map(p => [new RegExp(p[0], 'g'), p[1]])
   .map(p => `\n${sp(4)}.replace(${p[0]}, '${p[1]}')`)
   .join('')};`);
@@ -21,7 +21,7 @@ const toHtml = (element, strings = [], indentation = 0, insidePre = false, level
   let text = element.text || ((typeof element === `string`) ? element : undefined);
   if (text) {
 
-  	// TODO escape text inside `code`?
+    // TODO escape text inside `code`?
     writeIndented(insidePre ? text : escape(text));
     write('\n');
     return;
@@ -29,12 +29,12 @@ const toHtml = (element, strings = [], indentation = 0, insidePre = false, level
   writeIndented(`<${element.name} `);
   let attrsStr = Object.entries(element.attributes)
     .map(([k, v]) => `${k}="${v}"`)
-  	.join(` `);
+    .join(` `);
   write(`${attrsStr}>\n`);
   (element.children || []).map(el => toHtml(el, strings, indentation + 1, element.name === `pre`, level + 1));
   // TODO some tags optional close - check
   if (!(element.name in tagsNoClose)) {
-  	writeIndented(`</${element.name}>\n`);
+    writeIndented(`</${element.name}>\n`);
   }
   return strings.join('');
 };
